95f8f0a0d1ef1292ee986c27ffea955692b5a120
IlyaSukhodolskiy <ilya.sukhodolskiy@gmail.com>
2015-03-20 08:53:14 +0300

    refactoring code

--- README.md
+++ README.md
@@ -1,12 +1,64 @@
-# ICEBERG
+# ICEBERG (harvesting project)
+
+## Install and start
+
+1. ```$ npm install```
+2. ```$ gulp```
 
 
-### Install 
-```
-	$ npm install
-```
+## Gulp
 
-### Run gulp
-```
-	$ gulp
+1. gulp-autoprefixer"
+2. gulp-jsmin
+3. gulp-minify-css
+4. gulp-rename
+5. gulp-sass
+6. gulp-concat
+7. imagemin-svgo
+
+
+## CSS(SCSS) (12 columns)
+
+### Grid
+1. phones (<768px) ``` .col-ph-* ```
+2. tablets (≥768px) ``` .col-td-* ```
+3. desktops (≥992px) ``` .col-dt-* ```
+4. larger desktops (≥1200px) ``` .col-ld-* ```
+
+### Mixins
+
+1. @mixin opacity
+2. @mixin circle
+3. @mixin box-shadow
+4. @mixin vertical-align
+5. @mixin font-size
+6. @mixin gradient
+
+### Example create grid
+
+```HTML
+<div class="container">
+	<div class="row">
+		<div class="col-tb-6">
+			<!-- ... -->
+		</div>
+		<div class="col-tb-6">
+			<!-- ... -->
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-ph-4">
+			<!-- ... -->
+		</div>
+		<div class="col-tb-4">
+			<!-- ... -->
+		</div>
+		<div class="col-tb-4">
+			<!-- ... -->
+		</div>
+		<div class="col-tb-4">
+			<!-- ... -->
+		</div>
+	</div>
+</div>
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,46 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	minifyCSS = require('gulp-minify-css'),
-	rename = require('gulp-rename'),
-	jsmin = require('gulp-jsmin'),
-	concat = require('gulp-concat'),
-	svgo = require('imagemin-svgo'),
-  header = require('gulp-header'),
-  pkg = require('./package.json'),
-  banner = ['/*', ' <%= pkg.name %> - v<%= pkg.version %>', ' @description <%= pkg.description %>', 
-            ' --------------------', ' @author <%= pkg.author %>', ' @link <%= pkg.link %>', ' */',' '].join('\n');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    jsmin = require('gulp-jsmin'),
+    concat = require('gulp-concat'),
+    svgo = require('imagemin-svgo');
 
-gulp.task('scss', function () {
-    gulp.src('./src/scss/main.scss')
-    .pipe(sass({ includePaths: ['./scss'], errLogToConsole: true }))
- 	  .pipe(autoprefixer('last 10 version'))
-    .pipe(header(banner, { pkg : pkg } ))
-    .pipe(gulp.dest('./public/css'))
-    .pipe(minifyCSS())
-    .pipe(header(banner, { pkg : pkg } ))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./public/css'))
-});
-gulp.task('css', function () {
-    gulp.src(['./src/css/*'])
-    .pipe(minifyCSS())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(header(banner, { pkg : pkg } ))
-    .pipe(gulp.dest('./public/css/lib'))
-});
-gulp.task('js', function () {
-    gulp.src('./src/js/script.js')
-    .pipe(jsmin())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(header(banner, { pkg : pkg } ))
-    .pipe(gulp.dest('./public/js'))
-});
-gulp.task('img', function(){
-    gulp.src(['./src/images/*.*'])
-    .pipe(svgo()())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./public/images/'))
-});
+    gulp.task('scss', function () {
+        gulp.src('./src/scss/main.scss')
+        .pipe(sass({ includePaths: ['./scss'], errLogToConsole: true }))
+        .pipe(autoprefixer('last 10 version'))
+        .pipe(gulp.dest('./public/css'))
+        .pipe(minifyCSS())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./public/css'))
+    });
+    gulp.task('css', function () {
+        gulp.src(['./src/css/*'])
+        .pipe(minifyCSS())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./public/css/lib'))
+    });
+    gulp.task('js', function () {
+        gulp.src('./src/js/*')
+        .pipe(gulp.dest('./public/js'))
+        .pipe(jsmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./public/js'))
+    });
+    gulp.task('svg', function(){
+        gulp.src(['./src/images/*.svg'])
+        .pipe(svgo()())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./public/images/'))
+    });
+
 gulp.task('watch',function(){
-  gulp.watch('./src/scss/*.scss',['scss']),
-  gulp.watch(['./src/css/*.css','./src/css/lib/*.css'],['css']),
-  gulp.watch(['./src/js/*.js','./src/js/lib/*.js'],['js']),
-  gulp.watch(['./src/images/*.svg'],['img']);
+  gulp.watch('./src/scss/*',['scss']),
+  gulp.watch(['./src/css/*'],['css']),
+  gulp.watch(['./src/js/*'],['js']),
+  gulp.watch(['./src/images/*.svg'],['svg']);
 });
-gulp.task('default',['watch', 'scss', 'js', 'img', 'css']);
+
+gulp.task('default',['watch', 'scss', 'js', 'svg', 'css']);
